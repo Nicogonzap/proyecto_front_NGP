38cf6891db531b86110e36aa307e217fe1071192
Nicogonzap <nicolasgonzap@gmail.com>
2021-10-18 00:59:06 -0300

    Armado el home en HTML, agregada visualización en app.js, añadidas imagenes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let express = require ('express');
-let app = express();
+const express = require ('express');
+const { join } = require('path');
+const app = express();
+const path = require ('path');
+
+const publicPath = path.resolve(__dirname,'./public');
+// Esto indica que la carpeta public será un recurso de archivos estáticos
+app.use (express.static(publicPath));
 
 app.listen (3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
+})
+
+app.get ('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
